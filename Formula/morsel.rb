eea14326ddf66e7b4e3aa4365d1ed4bd25f2d607
Samuel Lee <samuel_lee@intuit.com>
2021-08-22 21:54:51 -0500

    update morsel

--- Formula/morsel.rb
+++ Formula/morsel.rb
@@ -2,7 +2,7 @@ class Morsel < Formula
   desc "CLI for translating Morse code input to text in real time."
   homepage "https://github.com/samlee514/morsel"
   url "https://github.com/SamLee514/morsel/releases/download/v0.1.0/morsel-mac.tar.gz"
-  sha256 "7d31094b1eba18610ae99732e7577c6d069c55799b866dcfa6f0da78c479c875"
+  sha256 "67fe29dbb99c13fe5cc9d8e34dedb4c150f0598fb83ad91c5c2cfcdd8fcc8a95"
   version "0.1.0"
 
   def install
